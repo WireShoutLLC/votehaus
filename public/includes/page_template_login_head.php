6da4670001eb970a3e6c34932823fabef43159a0
Jason Spriggs <jason@jasonspriggs.com>
2017-02-09 12:56:19 -0500

    Move iCheck blue css to CDN

--- public/includes/page_template_login_head.php
+++ public/includes/page_template_login_head.php
@@ -15,7 +15,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/flat/blue.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
