d5cfdf7cf656a2e87d0ad4f8c853e9a4904acbbd
Jason Spriggs <jason@jasonspriggs.com>
2017-02-09 08:57:48 -0500

    Move bootstrap to CDN

--- public/includes/page_template_login_foot.php
+++ public/includes/page_template_login_foot.php
@@ -4,7 +4,7 @@
 <!-- jQuery 2.2.3 -->
 <script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!-- iCheck -->
 <script src="plugins/iCheck/icheck.min.js"></script>
 <script>
