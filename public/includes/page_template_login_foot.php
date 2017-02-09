a717bae01e38064d4b660063e50504c6c3d376da
Jason Spriggs <jason@jasonspriggs.com>
2017-02-09 12:57:13 -0500

    Move iCheck and jQuery libs to CDN

--- public/includes/page_template_login_foot.php
+++ public/includes/page_template_login_foot.php
@@ -2,11 +2,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery 2.2.3 -->
-<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!-- iCheck -->
-<script src="plugins/iCheck/icheck.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
