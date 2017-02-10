887bc4db09dc04654e1df25e33f07b0ae9c80315
Jason Spriggs <jason@jasonspriggs.com>
2017-02-09 20:02:13 -0500

    Add dashboard template

--- public/includes/page_dashboard.php
+++ public/includes/page_dashboard.php
@@ -1,2 +1,39 @@
-<h1>Login success!</h1>
-<p><a href="/logout">Logout</a></p>
+<?php
+
+require_once('config.php');
+global $config;
+
+require_once('page_template_login_head.php');
+require_once('page_template_login_sidebar.php');
+
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Page Header
+        <small>Optional description</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Your Page Content Here -->
+     
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php 
+
+require_once('page_template_login_foot.php');
+
+?>
