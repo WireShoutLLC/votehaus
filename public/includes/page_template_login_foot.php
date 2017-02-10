397a59e75374c21b860f9d81572311f4ce8b0a40
Jason Spriggs <jason@jasonspriggs.com>
2017-02-09 19:27:04 -0500

    Include ajax login script

--- public/includes/page_template_login_foot.php
+++ public/includes/page_template_login_foot.php
@@ -4,5 +4,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script src="dist/js/login.js"></script>
 </body>
 </html>
