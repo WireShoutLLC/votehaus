1569e89f4fe97526bbce4ef41a64a1e914461c5b
jasonspriggs <jason@jasonspriggs.com>
2017-02-09 21:09:07 -0500

    Change fontawesome for sidebar

--- public/includes/page_template_dash_sidebar.php
+++ public/includes/page_template_dash_sidebar.php
@@ -4,9 +4,9 @@
 
       <ul class="sidebar-menu">
         <li class="header">ELECTIONS</li>
-        <li><a href="#"><i class="fa fa-link"></i> <span>Election 1</span></a></li>
-        <li><a href="#"><i class="fa fa-link"></i> <span>Election 2</span></a></li>
-        <li><a href="#"><i class="fa fa-link"></i> <span>New Election...</span></a></li>
+        <li><a href="#"><i class="fa fa-users"></i> <span>Election 1</span></a></li>
+        <li><a href="#"><i class="fa fa-users"></i> <span>Election 2</span></a></li>
+        <li><a href="#"><i class="fa fa-pencil-square-o"></i> <span>New Election...</span></a></li>
       </ul>
     </section>
   </aside>
