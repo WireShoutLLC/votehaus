2e0cd2dfa8bc69f5809ae24de56dd20d982762e9
Jason Spriggs <jason@jasonspriggs.com>
2017-02-09 20:16:13 -0500

    Move some libraries around and remove excess items

--- public/includes/page_template_dash_sidebar.php
+++ public/includes/page_template_dash_sidebar.php
@@ -2,41 +2,11 @@
 
     <section class="sidebar">
 
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p>Alexander Pierce</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
-
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-
       <ul class="sidebar-menu">
-        <li class="header">HEADER</li>
-        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-        <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#">Link in level 2</a></li>
-            <li><a href="#">Link in level 2</a></li>
-          </ul>
-        </li>
+        <li class="header">ELECTIONS</li>
+        <li><a href="#"><i class="fa fa-link"></i> <span>Election 1</span></a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span>Election 2</span></a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span>New Election...</span></a></li>
       </ul>
     </section>
   </aside>
