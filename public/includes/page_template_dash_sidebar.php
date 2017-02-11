fe801d327a622f8ac0371faa56c6b38b930c711b
Jason Spriggs <jason@jasonspriggs.com>
2017-02-11 18:31:48 -0500

    Begin adding election functionality - sidebar

--- public/includes/page_template_dash_sidebar.php
+++ public/includes/page_template_dash_sidebar.php
@@ -4,9 +4,9 @@
 
       <ul class="sidebar-menu">
         <li class="header">ELECTIONS</li>
-        <li><a href="#"><i class="fa fa-users"></i> <span>Election 1</span></a></li>
-        <li><a href="#"><i class="fa fa-users"></i> <span>Election 2</span></a></li>
-        <li><a href="#"><i class="fa fa-pencil-square-o"></i> <span>New Election...</span></a></li>
+        <li><a href="/elections?id=1"><i class="fa fa-users"></i> <span>Election 1</span></a></li>
+        <li><a href="/elections?id=2"><i class="fa fa-users"></i> <span>Election 2</span></a></li>
+        <li><a href="/elections?id=new"><i class="fa fa-pencil-square-o"></i> <span>New Election...</span></a></li>
       </ul>
     </section>
   </aside>
