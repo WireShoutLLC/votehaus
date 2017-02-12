5195bca6500fc521bfad6996e2518ac612eccbc3
Jason Spriggs <jason@jasonspriggs.com>
2017-02-12 16:38:32 -0500

    Add in some UI elements

--- public/includes/page_election_dashboard.php
+++ public/includes/page_election_dashboard.php
@@ -26,9 +26,71 @@ $election_name = $stmt->fetch(PDO::FETCH_NUM)[0];
     </section>
 
     <section class="content">
-
-      <!-- Your Page Content Here -->
-     
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Messages</span>
+              <span class="info-box-number">0</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box bg-green">
+            <span class="info-box-icon"><i class="fa fa-thumbs-o-up"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Votes Collected</span>
+              <span class="info-box-number">0</span>
+              <div class="progress">
+                <div class="progress-bar" style="width: 0%"></div>
+              </div>
+                  <span class="progress-description">
+                    0 out of 0 votes have been cast
+                  </span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Uploads</span>
+              <span class="info-box-number">0</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-star-o"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Likes</span>
+              <span class="info-box-number">0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="box box-widget widget-user-2">
+            <div class="widget-user-header bg-yellow">
+              <div class="widget-user-image">
+                <img class="img-circle" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
+              </div>
+              <h3 class="widget-user-username">Election Board</h3>
+              <h5 class="widget-user-desc">Who is overseeing this election?</h5>
+            </div>
+            <div class="box-footer no-padding">
+              <ul class="nav nav-stacked">
+                <li>Joyce Horton</li>
+                <li>Darnell Baldwin</li>
+                <li>Valerie Logan</li>
+                <li>Carolyn Kelley</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 
