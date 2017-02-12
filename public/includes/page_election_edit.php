2200e01324a999d4666afe87b381624aeb523e7c
Jason Spriggs <jason@jasonspriggs.com>
2017-02-12 13:49:26 -0500

    Add in election title to content pane

--- public/includes/page_election_edit.php
+++ public/includes/page_election_edit.php
@@ -6,17 +6,22 @@ global $config;
 require_once('page_template_dash_head.php');
 require_once('page_template_dash_sidebar.php');
 
+$stmt = $pdo->prepare("SELECT `name` FROM `elections` WHERE `id`= ?");
+$stmt->bindParam(1, $row["election"]);
+$stmt->execute();
+$election_name = $stmt->fetch(PDO::FETCH_NUM)[0];
+
 ?>
 
   <div class="content-wrapper">
     <section class="content-header">
       <h1>
         Edit Election
-        <small>Optional description</small>
+        <small><?php echo $election_name; ?></small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
+        <li><i class="fa fa-th-list"></i> Elections</li>
+        <li class="active"><?php echo $election_name; ?></li>
       </ol>
     </section>
 
