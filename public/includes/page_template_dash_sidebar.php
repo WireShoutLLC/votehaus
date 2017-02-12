67e7f21b6a4c97b37467fe6d05d04eed20260890
Jason Spriggs <jason@jasonspriggs.com>
2017-02-12 15:36:03 -0500

    Add sidebar bits

--- public/includes/page_template_dash_sidebar.php
+++ public/includes/page_template_dash_sidebar.php
@@ -18,13 +18,25 @@ $elections = $stmt->fetchAll();
       <ul class="sidebar-menu">
         <li class="header">ELECTIONS</li>
         <?php foreach ($elections as $row) {
-        	$stmt = $pdo->prepare("SELECT `name` FROM `elections` WHERE `id`= ?");
+        	$stmt = $pdo->prepare("SELECT `name`,`stage` FROM `elections` WHERE `id`= ?");
         	$stmt->bindParam(1, $row["election"]);
         	$stmt->execute();
         	
         	$election_name = $stmt->fetch(PDO::FETCH_NUM)[0];
+        	
+        	if($stage == "created")
+        		$infobox = '<span class="pull-right-container"><small class="label pull-right bg-purple">new</small></span>';
+        	else if($stage == "polling")
+        		$infobox = '<span class="pull-right-container"><small class="label pull-right bg-green">polling</small></span>';
+        	else if($stage == "done")
+        		$infobox = '<span class="pull-right-container"><small class="label pull-right bg-red">done</small></span>';
+        	else if($stage == "archived")
+        		$infobox = '<span class="pull-right-container"><small class="label pull-right bg-gray">archived</small></span>';
+        	else
+        		$infobox = '';
+        	
         	?>
-        	<li <?php if($_GET['id'] == $row['election']) { ?>class="active"<?php } ?>><a href="/election?id=<?php echo $row["election"]; ?>&subid=dashboard"><i class="fa fa-users"></i> <span><?php echo $election_name; ?></span></a></li>
+        	<li <?php if($_GET['id'] == $row['election']) { ?>class="active"<?php } ?>><a href="/election?id=<?php echo $row["election"]; ?>&subid=dashboard"><i class="fa fa-users"></i> <span><?php echo $election_name; ?></span><?php echo $infobox; ?></a></li>
         	<?php 
         } ?>
         <li <?php if($_GET['id'] == "new") { ?>class="active"<?php } ?>><a href="/election?id=new"><i class="fa fa-pencil-square-o"></i> <span>New Election...</span></a></li>
