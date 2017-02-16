1a8cf40dcdff24475de97a6ab0f70dbbff42aee0
Jason Spriggs <jason@jasonspriggs.com>
2017-02-16 08:38:01 -0500

    Remove profile bit for now, will be used later

--- public/includes/page_template_dash_head.php
+++ public/includes/page_template_dash_head.php
@@ -56,9 +56,6 @@
 		                				</p>
 		              				</li>
 		              				<li class="user-footer">
-		                				<div class="pull-left">
-		                  					<a href="#" class="btn btn-default btn-flat">Profile</a>
-		                				</div>
 		                				<div class="pull-right">
 		                  					<a href="/logout" class="btn btn-default btn-flat">Sign out</a>
 		                				</div>
