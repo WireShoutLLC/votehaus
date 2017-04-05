ed81dd5d79d66d3840340beb53532b24c60438d5
Jason Spriggs <jason@jasonspriggs.com>
2017-04-04 20:35:03 -0400

    Update process_new_election_admin.php

--- public/endpoints/process_new_election_admin.php
+++ public/endpoints/process_new_election_admin.php
@@ -11,16 +11,15 @@ if(isset($_POST['_csrf']) && session_csrf_check($_POST['_csrf'])) {
 		$email = $_POST['email'];
 		$election_id = $_POST['election'];
 		
-		$uid = session_get_user_id();
                 $uid_of_email = get_user_id($email);
-		$stmt = $pdo->prepare("INSERT INTO `access` (`election`, `user`, `level`) VALUES (?, ?, 254)");
-		$stmt->bindParam(1, $election_id);
-		$stmt->bindParam(2, $uid_of_email);
-		$stmt->execute();
-		log_auditable_action($uid, "make_elecadmin", $uid_of_email);
-		
-		$data['success'] = true;
-		$data['message'] = 'Success!';
+		if($uid_of_email != FALSE) {
+			make_user_elecadmin($uid_of_email, $election_id);
+
+			$data['success'] = true;
+			$data['message'] = 'Success!';
+		} else {
+			$errors['name'] = 'User does not exist.';
+		}
 	} else {
 		$errors['name'] = 'Invalid name.';
 	}
