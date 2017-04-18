060911b0a39d63440ae6177ede8d703ce300e1b2
Jason Spriggs <jason@jasonspriggs.com>
2017-04-17 22:40:45 -0400

    Base template for nomination acceptance page

--- public/includes/page_election_nominate_1_pending.php
+++ public/includes/page_election_nominate_1_pending.php
@@ -24,7 +24,36 @@ $election_name = get_election_name($election_id);
 	</section>
 	<section class="content">
 		<div class="row">
-			
+			<div class="col-lg-9 col-xs-12">
+				<!-- Questions -->
+				<div class="box box-primary">
+					<div class="box-header with-border">
+						<h3 class="box-title">Nomination Notice</h3>
+					</div>
+					<div class="box-body">
+						<h1>Congratulations! You have been nominated for a position.</h1>
+						<p>Please read the following notice from the Election Administrators:</p>
+						<p></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-xs-12">
+				<!-- Questions -->
+				<div class="box box-primary">
+					<div class="box-header with-border">
+						<h3 class="box-title">Nomination Acceptance</h3>
+					</div>
+					<div class="box-body">
+						<p>Would you like to accept this nomination and participate in this election?</p>
+						<form name="nomination_acceptance" id="nomination_acceptance" action="endpoints/process_election_nomination.php" method="post">
+							<?php csrf_render_html(); ?>
+							<input type="hidden" name="election_id" value="<?php echo $election_id; ?>" />
+							<button type="submit" name="accept" class="btn btn-block btn-success btn-lg">I Accept</button>
+							<button type="submit" name="decline" class="btn btn-block btn-danger btn-lg">I Decline</button>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 </div>
