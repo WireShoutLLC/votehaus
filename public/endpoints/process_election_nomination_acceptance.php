1da0c1c5789b0e32cd6ab6643849fbaf656f9739
Jason Spriggs <jason@jasonspriggs.com>
2017-04-20 21:12:53 -0400

    Fixes to variables for election nomination acceptance/decline

--- public/endpoints/process_election_nomination_acceptance.php
+++ public/endpoints/process_election_nomination_acceptance.php
@@ -5,7 +5,7 @@ $errors	= array();
 $data	= array();
 
 if(isset($_POST['_csrf']) && session_csrf_check($_POST['_csrf'])) {
-	if(isset($_POST['election_id']) && !empty($_POST['election_id']) && isset($_POST['accept']) || isset($_POST['decline'])) {
+	if(isset($_POST['election_id']) && !empty($_POST['election_id']) && (isset($_POST['accept']) || isset($_POST['decline']))) {
 		global $pdo;
 		if(isset($_POST['accept'])) {
 			set_user_election_access(get_current_user(), $_POST['election_id'], 101);
