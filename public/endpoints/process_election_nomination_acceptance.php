ff2086ff9fecf83566a182ec6b1fe2c6b82dc65b
Jason Spriggs <jason@jasonspriggs.com>
2017-04-20 22:10:12 -0400

    im gonna cry

--- public/endpoints/process_election_nomination_acceptance.php
+++ public/endpoints/process_election_nomination_acceptance.php
@@ -7,12 +7,12 @@ $data	= array();
 if(isset($_POST['_csrf']) && session_csrf_check($_POST['_csrf'])) {
 	if(isset($_POST['election_id']) && !empty($_POST['election_id'] && does_election_exist($_POST['election_id'])) && (isset($_POST['accept']) || isset($_POST['decline']))) {
 		if(isset($_POST['accept'])) {
-			set_user_election_access(get_current_user(), $_POST['election_id'], 101);
+			set_user_election_access(session_get_user_id(), $_POST['election_id'], 101);
 			
 			$data['success'] = true;
 			$data['message'] = 'Success!';
 		} else if(isset($_POST['decline'])) {
-			set_user_election_access(get_current_user(), $_POST['election_id'], 0);
+			set_user_election_access(session_get_user_id(), $_POST['election_id'], 0);
 			
 			$data['success'] = true;
 			$data['message'] = 'Success!';
