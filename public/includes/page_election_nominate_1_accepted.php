8f60d85e15025347ab72d95624a6a7a4954ce2c4
Jason Spriggs <jason@jasonspriggs.com>
2017-04-23 11:48:07 -0400

    Add question markers

--- public/includes/page_election_nominate_1_accepted.php
+++ public/includes/page_election_nominate_1_accepted.php
@@ -29,20 +29,22 @@ $election_name = get_election_name($election_id);
 					<div class="box-header with-border">
 						<h3 class="box-title">Voter Guide</h3>
 					</div>
-					<form>
+					<form name="nomination_questions" id="nomination_questions" action="endpoints/process_election_nomination_voter_answers.php" method="post">
 						<div class="box-body">
 							<?php 
 							$questions = get_questions_for_election($election_id); 
 							foreach($questions as $electionquestion) {
 								$questiondata = json_decode($electionquestion['data'], true);
 								if($questiondata['type'] == "nominee_1") {
+									$qnum = 0;
 									$voter_guide = $questiondata['data']['voter_guide']; 
 									foreach($voter_guide as $question) { ?>
 									<div class="form-group">
 										<label><?php echo $question['question']; ?></label>
-										<textarea class="form-control" rows="3" maxlength="<?php echo $question['limit']; ?>" placeholder="Input your answer here"></textarea>
+										<textarea class="form-control" name="question-<?php echo $qnum; ?>" rows="3" maxlength="<?php echo $question['limit']; ?>" placeholder="Input your answer here"></textarea>
 									</div>
 									<?php
+									$qnum++;
 									}
 								}
 							} ?>
