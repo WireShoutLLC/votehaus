7bf73ef00d8912010aa20a93128ca495d35630c3
Jason Spriggs <jason@jasonspriggs.com>
2017-04-23 15:41:09 -0400

    Add in functionality

--- public/endpoints/process_election_nomination_voter_answers.php
+++ public/endpoints/process_election_nomination_voter_answers.php
@@ -7,7 +7,8 @@ $data	= array();
 if(isset($_POST['_csrf']) && session_csrf_check($_POST['_csrf'])) {
 	if(isset($_POST['election_id']) && !empty($_POST['election_id'] && does_election_exist($_POST['election_id'])) {
 		$election_id = $_POST['election_id'];
-		$questions = get_questions_for_election($election_id); 
+		$voterguidedata = array("voter_guide" => array());
+		$questions = get_questions_for_election($election_id);
 		foreach($questions as $electionquestion) {
 			$questiondata = json_decode($electionquestion['data'], true);
 			if($questiondata['type'] == "nominee_1") {
@@ -18,12 +19,15 @@ if(isset($_POST['_csrf']) && session_csrf_check($_POST['_csrf'])) {
 					if(isset($_POST[$questionform]) && strlen($_POST[$questionform]) <= $question['limit']) {
 						$descript = htmlspecialchars($_POST[$questionform]);
 						
-						//TODO: Add to user access data voter guide array
+						$voterguidedata['voter_guide'][$qnum] = $descript;
 					}
 					$qnum++;
 				}
 			}
 		}
+		set_user_election_access(session_get_user_id(), $election_id, 101, json_encode($voterguidedata));
+		$data['success'] = true;
+		$data['message'] = 'Success!';
 	} else {
 		$errors['name'] = 'Invalid election.';
 	}
