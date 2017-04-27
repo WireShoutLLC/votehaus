d8b5dfc15775f90451d84e2016941f369bb083f2
Jason Spriggs <jason@jasonspriggs.com>
2017-04-26 23:17:52 -0400

    Voter Sidebar

---
+++ public/includes/page_template_voter_sidebar.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('config.php');
+global $config;
+
+$election_id = get_voter_election_id();
+?>
+<aside class="main-sidebar">
+	<section class="sidebar">
+		<ul class="sidebar-menu">
+			<li class="header">ELECTIONS</li>
+		</ul>
+    	</section>
+</aside>
