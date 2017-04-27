3a12ada49b0533148a986a40bf863c8b0d023aad
Jason Spriggs <jason@jasonspriggs.com>
2017-04-27 19:31:31 -0400

    Fix header links

--- public/includes/page_template_voter_head.php
+++ public/includes/page_template_voter_head.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>votehaus | Dashboard</title>
+		<title>votehaus | Vote</title>
 		<!-- Tell the browser to be responsive to screen width -->
 		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 		<!-- Bootstrap 3.3.6 -->
@@ -39,7 +39,7 @@
 	<body class="hold-transition skin-blue sidebar-mini">
 		<div class="wrapper">
 			<header class="main-header">
-		  		<a href="/dashboard" class="logo">
+		  		<a href="/vote" class="logo">
 			    	<span class="logo-mini"><b>v</b>h</span>
 			    	<span class="logo-lg"><b>vote</b>haus</span>
 		    	</a>
