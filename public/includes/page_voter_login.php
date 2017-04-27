6b8cad5e4a3b2efa4fa4481663fc6e43e052cdcb
Jason Spriggs <jason@jasonspriggs.com>
2017-04-26 21:20:56 -0400

    ID add

--- public/includes/page_voter_login.php
+++ public/includes/page_voter_login.php
@@ -20,9 +20,9 @@ global $config;
 			<form id="voterloginbox" class="lockscreen-credentials" action="endpoints/process_voter_login.php" method="post">
 				<div class="input-group">
 					<?php csrf_render_html(); ?>
-					<input name="voter_token" type="password" class="form-control" placeholder="password" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;" autocomplete="off">
+					<input name="voter_token" id="voter_token" type="password" class="form-control" placeholder="password" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;" autocomplete="off">
 					<div class="input-group-btn">
-						<a href="#" class="btn" data-toggle="modal" data-target="#verify"><i class="fa fa-arrow-right text-muted"></i></a>
+						<a href="#" id="next_btn" class="btn" data-toggle="modal" data-target="#verify"><i class="fa fa-arrow-right text-muted"></i></a>
 					</div>
 					<div class="modal fade" id="verify" tabindex="-1" role="dialog" aria-labelledby="voterLoginLabel">
 						<div class="modal-dialog" role="document">
