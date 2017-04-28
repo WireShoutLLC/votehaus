e5db3e22985e2d25cd70a31d93b0bd47cd537d9f
Jason Spriggs <jason@jasonspriggs.com>
2017-04-27 20:37:32 -0400

    Add nominees to sidebar

--- public/includes/page_template_voter_sidebar.php
+++ public/includes/page_template_voter_sidebar.php
@@ -4,11 +4,19 @@ require_once('config.php');
 global $config;
 
 $election_id = get_voter_election_id();
+$election_nominees = get_election_users_at_access($election_id, 101);
+
 ?>
+
 <aside class="main-sidebar">
 	<section class="sidebar">
 		<ul class="sidebar-menu">
-			<li class="header">ELECTIONS</li>
+			<li class="header">Candidates</li>
+			<?php foreach($election_nominees as $nominee) { 
+			$voter_guide = json_decode($nominee['data'], true)['voter_guide']; ?>
+			<li><a href="#<?php echo $nominee['user']; ?>"><span><?php echo $voter_guide[0]; ?></span></a></li>
+			<?php } ?>
+			<li class="header">Ballot</li>
 		</ul>
     	</section>
 </aside>
