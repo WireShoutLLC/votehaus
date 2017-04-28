a8addda68e413e7d0006868239fac14ea3f6c5a6
Jason Spriggs <jason@jasonspriggs.com>
2017-04-27 20:06:18 -0400

    Add page_template_voter_foot.php

---
+++ public/includes/page_template_voter_foot.php
@@ -0,0 +1,14 @@
+			<footer class="main-footer">
+				<div class="pull-right hidden-xs">
+			    		<a href="https://github.com/wireshoutllc/votehaus">Source</a>
+				</div>
+				<strong>Copyright &copy; <?php echo date("Y"); ?> <a href="https://wireshout.com">WireShout, LLC.</a>.</strong> All rights reserved.
+			</footer>
+		</div>
+		
+		<!-- Bootstrap 3.3.6 -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- AdminLTE App -->
+		<script src="dist/js/app.min.js"></script>
+	</body>
+</html>
