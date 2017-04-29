35e0b9a9e7ee95cd690b1f7712afd2c7d2dbe06c
Jason Spriggs <jason@jasonspriggs.com>
2017-04-29 12:11:07 -0400

    Add things

--- public/includes/page_voter_ballot.php
+++ public/includes/page_voter_ballot.php
@@ -28,20 +28,23 @@ $election_nominees = get_election_users_at_access($election_id, 101);
 			<div class="col-xs-12">
 				<div class="box">
 					<div class="box-header with-border">
-						<h3 class="box-title"><i class="fa fa-tag"></i> Ballot</h3>
+						<h3 class="box-title"><i class="fa fa-check-circle-o"></i> Ballot</h3>
 					</div>
 					<div class="box-body">
 						<div class="alert alert-info">
 							<h4><i class="icon fa fa-info"></i> Instructions</h4>
-							This election uses the <a href="https://www.youtube.com/watch?v=3Y3jE3B8HsE&t=2s">alternative vote</a> method. To vote, drag the names of the candidates you would like to vote for ABOVE the --ORDER ABOVE HERE-- line, then order your chosen candidates from top to bottom by preferance, with the top being your favorite candidate and the bottom your least. If you do not want to vote for a candidate, leave them below the --ORDER ABOVE HERE-- line.
+							This election uses the <a href="https://www.youtube.com/watch?v=3Y3jE3B8HsE&t=2s">alternative vote</a> method. To vote, drag the names of the candidates you would like to vote for ABOVE the --ORDER ABOVE HERE-- line, then order your chosen candidates from top to bottom by preference, with the top being your favorite candidate and the bottom your least. If you do not want to vote for a candidate, leave them below the --ORDER ABOVE HERE-- line.
 						</div>
-						<ul id="candidates" class="list-group">
-							<li class="list-group-item">--ORDER ABOVE HERE--</li>
-							<?php foreach($election_nominees as $nominee) { 
-							$voter_guide = json_decode($nominee['data'], true)['voter_guide']; ?>
-							<li class="list-group-item" id="<?php echo $nominee['user']; ?>"><?php echo $voter_guide[0]; ?></li>
-							<?php } ?>
-						</ul>
+						<form name="ballot_response" id="ballot_response" action="endpoints/process_ballot.php" method="post">
+							<ul id="candidates" class="list-group">
+								<li class="list-group-item"><b>--ORDER ABOVE HERE--</b></li>
+								<?php foreach($election_nominees as $nominee) { 
+								$voter_guide = json_decode($nominee['data'], true)['voter_guide']; ?>
+								<li class="list-group-item" id="<?php echo $nominee['user']; ?>" name="<?php echo $nominee['user']; ?>"><?php echo $voter_guide[0]; ?></li>
+								<?php } ?>
+							</ul>
+							<button type="submit" class="btn btn-info pull-right">Submit Ballot</button>
+						</form>
 
 						<script>
 						Sortable.create(candidates, { group: "ballotgroup" });
