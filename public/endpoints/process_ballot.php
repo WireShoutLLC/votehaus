eae5f1c7e70ca6d750593becbbddfb7960196f52
Jason Spriggs <jason@jasonspriggs.com>
2017-04-29 13:46:13 -0400

    Do the thing

--- public/endpoints/process_ballot.php
+++ public/endpoints/process_ballot.php
@@ -5,10 +5,11 @@ $errors	= array();
 $data	= array();
 
 if(isset($_POST['_csrf']) && session_csrf_check($_POST['_csrf'])) {
-	if(isset($_POST['ballot'])) {
+	if(isset($_POST['ballot']) && !has_voter_voted()) {
 		$election_id = get_voter_election_id();
-		echo print_r($_POST['ballot'], true);
-		
+		$json = json_encode($_POST['ballot']);
+		$ballot = array($json);
+		record_ballot($election_id, $ballot);
 		
 		$data['success'] = true;
 		$data['message'] = 'Success!';
