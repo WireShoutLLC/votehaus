7063e360237cbe6f674762f54861cee271bd7e9a
Jason Spriggs <jason@jasonspriggs.com>
2017-04-29 13:13:30 -0400

    echo out ballot

---
+++ public/endpoints/process_ballot.php
@@ -0,0 +1,27 @@
+<?php
+require_once('../includes/config.php');
+
+$errors	= array();
+$data	= array();
+
+if(isset($_POST['_csrf']) && session_csrf_check($_POST['_csrf'])) {
+	if(isset($_POST['ballot'])) {
+		$election_id = get_voter_election_id();
+		echo $_POST['ballot'];
+		
+		
+		$data['success'] = true;
+		$data['message'] = 'Success!';
+	} else {
+		$errors['name'] = 'Invalid election.';
+	}
+} else {
+	$errors['req'] = 'Request is invalid.';
+}
+
+if(!empty($errors)) {
+	$data['success'] = false;
+	$data['errors']  = $errors;
+}
+
+echo json_encode($data);
