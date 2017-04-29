809d4df7f80521bd1bae0969859250791e8d153e
Jason Spriggs <jason@jasonspriggs.com>
2017-04-28 22:13:23 -0400

    This might suck?

--- public/includes/page_voter_dashboard.php
+++ public/includes/page_voter_dashboard.php
@@ -8,6 +8,7 @@ require_once('page_template_voter_sidebar.php');
 
 $election_id = get_voter_election_id();
 $election_name = get_election_name($election_id);
+$election_nominees = get_election_users_at_access($election_id, 101);
 
 ?>
 
@@ -24,7 +25,29 @@ $election_name = get_election_name($election_id);
 	</section>
 	<section class="content">
 		<div class="row">
-			
+			<div class="col-xs-12">
+				<?php foreach($election_nominees as $nominee) { 
+				$nom_voter_guide = json_decode($nominee['data'], true)['voter_guide']; ?>
+				<div class="box box-default" id="<?php echo $nominee['user']; ?>">
+					<div class="box-body">
+						<?php 
+						$questions = get_questions_for_election($election_id); 
+						foreach($questions as $electionquestion) {
+							$questiondata = json_decode($electionquestion['data'], true);
+							if($questiondata['type'] == "nominee_1") {
+								$voter_guide = $questiondata['data']['voter_guide'];
+								foreach($voter_guide as $question) { ?>
+								<h3><?php echo $question['question']; ?></h3>
+								<p><?php echo $nom_voter_guide[0]; ?></p>
+								<hr />
+								<?php
+								}
+							}
+						} ?>
+					</div>
+				</div>
+				<?php } ?>
+			</div>
 		</div>
 	</section>
 </div>
