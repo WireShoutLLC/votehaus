93a4ee5cd75c6e090cc56ee9e00a1e47fd35c73f
Jason Spriggs <jason@jasonspriggs.com>
2017-04-29 11:57:02 -0400

    Generate ballot candidates

--- public/includes/page_voter_ballot.php
+++ public/includes/page_voter_ballot.php
@@ -26,40 +26,21 @@ $election_nominees = get_election_users_at_access($election_id, 101);
 	<section class="content">
 		<div class="row">
 			<div class="col-xs-12">
-				<div class="alert alert-info">
-					<h4><i class="icon fa fa-info"></i> Instructions</h4>
-					This election uses the single transferable voting method. To vote, drag the names of the candidates you would like to vote for to the ballot, then order them from top to bottom by preferance, with the top being your favorite candidate and the bottom your least. If you do not want to vote for a candidate, leave them off of your ballot.
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-6 col-xs-12">
-				<!-- Ballot -->
 				<div class="box">
 					<div class="box-header with-border">
 						<h3 class="box-title"><i class="fa fa-tag"></i> Ballot</h3>
 					</div>
 					<div class="box-body">
-						<ul id="ballot" class="list-group">
-							<li class="list-group-item filter">--IGNORE--<li>
-						</ul>
-
-						<script>
-						Sortable.create(ballot, { group: "ballotgroup" });
-						</script>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-6 col-xs-12">
-				<!-- Options -->
-				<div class="box">
-					<div class="box-header with-border">
-						<h3 class="box-title"><i class="fa fa-tag"></i> Candidate Options</h3>
-					</div>
-					<div class="box-body">
+						<div class="alert alert-info">
+							<h4><i class="icon fa fa-info"></i> Instructions</h4>
+							This election uses the <a href="https://www.youtube.com/watch?v=3Y3jE3B8HsE&t=2s">alternative vote</a> method. To vote, drag the names of the candidates you would like to vote for ABOVE the --ORDER ABOVE HERE-- line, then order your chosen candidates from top to bottom by preferance, with the top being your favorite candidate and the bottom your least. If you do not want to vote for a candidate, leave them below the --ORDER ABOVE HERE-- line.
+						</div>
 						<ul id="candidates" class="list-group">
-							<li class="list-group-item">This is <a href="http://rubaxa.github.io/Sortable/">Sortable</a></li>
-							<li class="list-group-item">It works with Bootstrap...</li>
+							<li class="list-group-item">--ORDER ABOVE HERE--</li>
+							<?php foreach($election_nominees as $nominee) { 
+							$voter_guide = json_decode($nominee['data'], true)['voter_guide']; ?>
+							<li class="list-group-item" id="<?php echo $nominee['user']; ?>"><?php echo $voter_guide[0]; ?></li>
+							<?php } ?>
 						</ul>
 
 						<script>
