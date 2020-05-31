28d4c2493c40e12a756d1c501e643cb3711c53b5
Jason Spriggs <jason@jasonspriggs.com>
2020-05-30 22:02:05 -0400

    Fix path

--- public/includes/credentials.php
+++ public/includes/credentials.php
@@ -4,5 +4,5 @@ $config['captcha']['pub']       = getenv('RECAPTCHA_PUBLIC');
 $config['captcha']['priv']      = getenv('RECAPTCHA_SECRET');
 
 $url = getenv('CLEARDB_DATABASE_URL');
-$dsn = parse_url($url, PHP_URL_SCHEME) . ':dbname=' . parse_url($url, PHP_URL_PATH) . ';host=' . parse_url($url, PHP_URL_HOST);
+$dsn = parse_url($url, PHP_URL_SCHEME) . ':dbname=' . substr(parse_url($url, PHP_URL_PATH), 1) . ';host=' . parse_url($url, PHP_URL_HOST);
 $pdo = new PDO($dsn, parse_url($url, PHP_URL_USER), parse_url($url, PHP_URL_PASS));
