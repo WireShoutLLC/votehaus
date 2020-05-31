79c57b2ced8740f08556ad12bfe6c1c3b02334eb
Jason Spriggs <jason@jasonspriggs.com>
2020-05-30 21:59:19 -0400

    Add updates to credentials.php

--- public/includes/credentials.php
+++ public/includes/credentials.php
@@ -3,4 +3,6 @@
 $config['captcha']['pub']       = getenv('RECAPTCHA_PUBLIC');
 $config['captcha']['priv']      = getenv('RECAPTCHA_SECRET');
 
-$pdo = new PDO(getenv('CLEARDB_DATABASE_URL'));
+$url = getenv('CLEARDB_DATABASE_URL');
+$dsn = parse_url($url, PHP_URL_SCHEME) . ':dbname=' . parse_url($url, PHP_URL_PATH) . ';host=' . parse_url($url, PHP_URL_HOST);
+$pdo = new PDO($dsn, parse_url($url, PHP_URL_USER), parse_url($url, PHP_URL_PASS));
